e50ce976627f639dfc634c66db51a35eb4cf26ba
IS2511 <is2511@bk.ru>
2019-09-21 12:53:03 +0300

    Moving changes from different files to one repo

--- task-1.2.4.lua
+++ task-1.2.4.lua
@@ -1,10 +1,16 @@
 
+-- NOTE: This is a WIP! *gets the black and yellow WIP tape*
+-- WIP WIP WIP WIP WIP WIP WIP WIP WIP WIP 
 
-box1_input = { x1 = 0, y1 = 0, x2 = 0, y2 = 0 }
-box2_input = { x1 = 0, y1 = 0, x2 = 0, y2 = 0 }
+t = {}
+debug = {}
+
+
+box1_input = { x1 = 20, y1 = 20, x2 = 120, y2 = 120 }
+box2_input = { x1 = 200, y1 = 200, x2 = 300, y2 = 280 }
 
 function create_box (x1, y1, x2, y2)
-  local box = {}
+  local box = { type = "box" }
   if (x1 < x2) then
     box.x1, box.x2, box.x3, box.x4 = x1, x1, x2, x2
   else
@@ -31,24 +37,82 @@ box2 = create_box(box2_input.x1, box2_input.y1, box2_input.x2, box2_input.y2)
   |                |
   1----------------4
 
-]]
+--]]
 
--- Considering this function will work with floats in the future
+-- Considering these functions will work with floats in the future
 -- I'm not adding >= or <= as there is no point in that (pun intended)
+
 function check_pointInside (x, y, box)
   return (((x > box.x1)and(x < box.x3)) and ((y > box.x2)and(y < box.x4)))
 end
 
-function check_boxCollision (b1, b2)
-  for i=1,4 do
-    if check_pointInside(b1["x"..i], b1["y"..i], b2) then
+-- function check_polygonRadius (poly1, poly2)
+--   --
+-- end
+-- function check_boxRadius (b1, b2)
+--   local x1, y1 = (b1.x3-b1.x1)
+-- end
+
+function check_linesCross (x11, y11, x12, y12, x21, y21, x22, y22)
+  -- 4 occasions (90 deg turns)
+  -- local m1, m2 = ((y12-y11)/(x12-x11)), ((y22-y21)/(x22-x21))
+  -- -- local f1, f2 = (x*m1 - x1*m1 + y1), (x*m2 - x2*m2 + y2)
+  -- local k1, k2 = m, m
+  -- local b1, b2 = (y1 - x1*m1), (y1 - x1*m2)
+  -- --   y - y1 = m*(x - x1)
+  -- --   y = x*m - x1*m + y1
+  -- --   k = m
+  -- --   b = y1 - x1*m
+  local p_down = (x11-x12)*(y21-y22) - (y11-y12)*(x21-x22)
+  local px = ((x11*y12-y11*x12)*(x21-x22)-(x11-x12)*(x21*y22-y21*x22))/p_down
+  local py = ((x11*y12-y11*x12)*(y21-y22)-(y11-y12)*(x21*y22-y21*x22))/p_down
+
+  local between = function (a, p, b)
+    return (  ( (a < p) and (p < b) )  or  ( (b < p) and (p < a) )  )
+  end
+
+  local cross = (  (between(x11, px, x12)) and (between(y11, px, y12))  ) or (  (between(x21, py, x22)) and (between(y21, py, y22))  )
+
+  if cross then
+    return true, {px, py}
+  else
+    return false
+  end
+
+  -- return false
+
+end
+
+-- function check_boxCollision (b1, b2)
+--   for i=1,4 do
+--     if check_pointInside(b1["x"..i], b1["y"..i], b2) then
+--       for j=1,4 do
+--         if check_pointInside(b2["x"..j], b2["y"..j], b1) then
+--           return true, {{x = b1["x"..i], y = b2["y"..j]}, {x = b2["x"..i], y = b1["y"..j]}}
+--         end
+--       end
+--       -- error("Something weird happend, invalid box")
+--     end
+--   end
+--   return false
+-- end
+function check_polygonCollision (poly1, poly2)
+  local points = {}
+  if (poly1.type == "box") and (poly2.type == "box") then
+    debug.type = "box"
+    -- if not check_boxRadius(poly1, poly2) then debug.r = "box radius failed"; return nil end
+    for i=1,4 do
       for j=1,4 do
-        if check_pointInside(b2["x"..j], b2["y"..j], b1) then
-          return true, {{x = b1["x"..i], y = b2["y"..j]}, {x = b2["x"..i], y = b1["y"..j]}}
+        local yes, point = check_linesCross(poly1["x"..i], poly1["y"..i], poly1["x"..(i+1)], poly1["y"..(i+1)], poly2["x"..j], poly2["y"..j], poly2["x"..(j+1)], poly2["y"..(j+1)])
+        if yes then
+          table.insert(points, point)
+          debug.point = "box"
+          -- table.insert(debug, point)
         end
       end
-      error("Something weird happend, invalid box")
     end
+    return points
   end
-  return false
+  return nil
 end
+
