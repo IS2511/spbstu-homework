90e301201e2435dd28e2de79f872176e6d001da4
IS2511 <is2511@bk.ru>
2019-10-29 13:55:06 +0300

    Fixed task 1.2.9

--- task-1.2.9.lua
+++ task-1.2.9.lua
@@ -4,27 +4,30 @@ local bit = require "bit"
 local arg_check = true
 local num1, num2 = 0, 0
 
-if type(tonumber(arg[1])) ~= "number" or type(tonumber(arg[2])) ~= "number" then
+if arg[1] == nil or arg[2] == nil then
   arg_check = false
+else
+  num1 = tonumber(arg[1])
+  num2 = tonumber(arg[2])
+  if num1 < 0 or num2 < 0 then arg_check = false end
+  if num1%1 ~= 0 or num2%1 ~= 0 then arg_check = false end
 end
 
-num1 = tonumber(arg[1])
-num2 = tonumber(arg[2])
-
-if num1%1 ~= 0 or num2%1 ~= 0 then arg_check = false end
-
 if not arg_check then
-  print("Comparing 2 integer numbers without the compare operator.")
+  print("Comparing 2 positive integer numbers without the compare operator.")
   print("Usage: "..arg[0].." <player-ATK> <enemy-ATK>")
   os.exit()
 end
 
-local enemy_bit = bit.rshift((num1 - num2), 31) == 1
-local player_bit = bit.rshift((num2 - num1), 31) == 1
-if (not enemy_bit) and (not player_bit) then
-  print("It's a draw")
-elseif enemy_bit then
-  print("Enemy deals damage to the player")
-elseif player_bit then
-  print("Player deals damage to the enemy")
+local enemy_attack = bit.rshift((num1 - num2), 31)
+local player_attack = bit.rshift((num2 - num1), 31)
+
+local atk1, atk2 = 0, 0
+for i=0,31 do
+  atk1 = atk1 + bit.band(num1, bit.lshift(player_attack, i))
+  atk2 = atk2 + bit.band(num2, bit.lshift(enemy_attack, i))
 end
+
+local result = atk1 + atk2
+
+print(result)
